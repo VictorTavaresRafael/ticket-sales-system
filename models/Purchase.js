0affd3e55d2396b180124a7efdd3cff823f75cda
Víctor Tavares <victor.tavares.rafael@gmail.com>
2025-02-11 15:07:12 -0300

    Atualização no histórico de compra
    Tentativa de ajustar falhou novamente, mas estou chegando próximo à solução.
    (Código comentado deve ser analisado na próxima atualização)

--- models/Purchase.js
+++ models/Purchase.js
@@ -1,10 +1,16 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+// const Ticket = require('./Ticket'); 
 
 const Purchase = sequelize.define('Purchase', {
   userId: DataTypes.INTEGER,
   ticketId: DataTypes.INTEGER,
   quantity: DataTypes.INTEGER,
 });
+
+// Associação com o modelo Ticket
+// Purchase.associate = (models) => {
+//   Purchase.belongsTo(models.Ticket, { foreignKey: 'ticketId' });
+// };
 
 module.exports = Purchase;
--- models/Ticket.js
+++ models/Ticket.js
@@ -1,11 +1,16 @@
-// models/Ticket.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+// const Purchase = require('./Purchase'); // Importe o modelo Purchase
 
 const Ticket = sequelize.define('Ticket', {
   name: DataTypes.STRING,
   price: DataTypes.FLOAT,
   quantity: DataTypes.INTEGER,
 });
+
+// Associação com o modelo Purchase
+// Ticket.associate = (models) => {
+//   Ticket.hasMany(models.Purchase, { foreignKey: 'ticketId' });
+// };
 
 module.exports = Ticket;
