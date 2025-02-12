a0eb30c3bd6f463f6d716cc0eadf8150cee5d937
Víctor Tavares <victor.tavares.rafael@gmail.com>
2025-02-12 15:57:23 -0300

    Registro e Login em ambiente web
    Processo de registro de novos usuário e acesso do sistema está funcionando corretamente, graças a Deus, nosso salvador!

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,14 @@
-// middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
 
 exports.authenticate = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token) return res.status(401).json({ message: 'Acesso negado' });
+  const token = req.cookies.token;
+  if (!token) return res.redirect('/auth/login'); // Redireciona para o login se não houver token
 
   try {
     const verified = jwt.verify(token, 'secretkey');
     req.user = verified;
     next();
   } catch (error) {
-    res.status(400).json({ message: 'Token inválido' });
+    res.redirect('/auth/login'); // Redireciona para o login se o token for inválido
   }
 };
--- routes/web/ticketRoutes.js
+++ routes/web/ticketRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const Ticket = require('../../models/Ticket');
+const { authenticate } = require('../../middleware/authMiddleware');
 const authMiddleware = require('../../middleware/authMiddleware');
 
-router.get('/tickets', async (req, res) => {
+router.get('/tickets', authenticate, async (req, res) => {
     const tickets = await Ticket.findAll();
     res.render('tickets', { tickets });
-});
+  });
 
 router.get('/purchase-history', authMiddleware, async (req, res) => {
     const purchases = await Purchase.findAll({ where: { userId: req.user.userId }, include: Ticket });
